3d95b5f0e0eea10be0b6d619782f443e07a2384e
cxb1988 <283817727@qq.com>
2020-08-21 21:26:35 +0800

    客服

--- pages/customer/customer.vue
+++ pages/customer/customer.vue
@@ -17,7 +17,7 @@ export default {
             plusrequire:"none", //禁止远程网页使用plus的API，有些使用mui制作的网页可能会监听plus.key，造成关闭页面混乱，可以通过这种方式禁止
             top:uni.getSystemInfoSync().statusBarHeight //放置在titleNView下方。如果还想在webview上方加个地址栏的什么的，可以继续降低TOP值
         })
-        wv.loadURL("http://live.easyliao.com/live/chat.do?c=11122&g=26881&config=40987&tag=0")
+        wv.loadURL("http://kefu.fangpaiwang.com/app/index.php?i=4&c=entry&toopenid=o65dpxLowyPNCRXWYWXuryA5UOkY&do=chat&m=cy163_customerservice")
         var currentWebview = this.$scope.$getAppWebview(); //此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效，非v3编译模式使用this.$mp.page.$getAppWebview()
         currentWebview.append(wv);//一定要append到当前的页面里！！！才能跟随当前页面一起做动画，一起关闭
         setTimeout(function() {
