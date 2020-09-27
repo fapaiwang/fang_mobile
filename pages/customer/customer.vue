b7ef5165bcbf12415227c859d4b078759101c8e3
cxb1988 <283817727@qq.com>
2020-09-27 09:13:29 +0800

    聊天

--- pages/customer/customer.vue
+++ pages/customer/customer.vue
@@ -1,11 +1,5 @@
 <template>
 	<view>
-		<view class="header-wrap" style="position: fixed;top: 0;width: 100%;display: flex;text-align: center;justify-content: center;z-index: 1;background: #fff;height: 80upx;line-height: 80upx;font-size: 32upx;">
-			<view class="addressView" @click="goBack" style="width: 58upx;position: absolute;left: 10upx;height: 34upx;">
-				<image class="arrowIcon" src='../../static/img/base/left.png' style="width: 18upx;height: 34upx;"></image>
-			</view>
-			<view style="text-align: center;text-indent:-10upx">客服</view>
-		</view>
 		<!-- <view class="bg">
 			<image src="../../static/img/base/bg.png" class="bgImg"></image>
 		</view>
@@ -13,10 +7,10 @@
 			 <text class="desc">程序猿小哥哥正在开发~</text>
 		</view> -->
 		<!-- #ifdef H5 -->
-		<web-view src="http://live.easyliao.com/live/chat.do?c=11122&g=26881&config=40987&tag=0"></web-view>
+		<web-view src="https://kefu.fangpaiwang.com/app/index.php?i=4&c=entry&toopenid=o65dpxAMaQiKzBsr_1tyFTmJDBn4&do=chat&m=cy163_customerservice"></web-view>
 		<!-- #endif -->
 		<!-- #ifdef MP-WEIXIN -->
-		<web-view src="https://live.easyliao.com/live/chat.do?c=11122&g=26881&config=40987&tag=0"></web-view>
+		<web-view src="https://kefu.fangpaiwang.com/app/index.php?i=4&c=entry&toopenid=o65dpxAMaQiKzBsr_1tyFTmJDBn4&do=chat&m=cy163_customerservice"></web-view>
 		<!-- #endif -->
 	</view>
 </template>
@@ -34,38 +28,20 @@
 				plusrequire: "none", //禁止远程网页使用plus的API，有些使用mui制作的网页可能会监听plus.key，造成关闭页面混乱，可以通过这种方式禁止
 				top: uni.getSystemInfoSync().statusBarHeight //放置在titleNView下方。如果还想在webview上方加个地址栏的什么的，可以继续降低TOP值
 			})
-			wv.loadURL("http://live.easyliao.com/live/chat.do?c=11122&g=26881&config=40987&tag=0")
+			wv.loadURL("https://kefu.fangpaiwang.com/app/index.php?i=4&c=entry&toopenid=o65dpxAMaQiKzBsr_1tyFTmJDBn4&do=chat&m=cy163_customerservice")
 			var currentWebview = this.$scope.$getAppWebview(); //此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效，非v3编译模式使用this.$mp.page.$getAppWebview()
 			currentWebview.append(wv); //一定要append到当前的页面里！！！才能跟随当前页面一起做动画，一起关闭
 			setTimeout(function() {
 				console.log(wv.getStyle())
 			}, 1000); //如果是首页的onload调用时需要延时一下，二级页面无需延时，可直接获取
 			// #endif
-		}
+		},
+		onNavigationBarButtonTap(e) {
+			uni.navigateBack({
+				delta:1
+			})	
+		},
 	};
 </script>
-
 <style scoped>
-	/* 	.bg{
-		position: relative;
-		height: 100%;
-		overflow: hidden;
-	}
-	.bgImg{
-		width: 750upx;
-		height: 480upx;
-	}
-	.desc{
-		width:100%;
-		height:35upx;
-		font-size:36upx;
-		font-family:Adobe Heiti Std;
-		font-weight:normal;
-		color:rgba(51,51,51,1);
-		line-height:38upx;
-		text-align: center;
-		justify-content: center;
-		display: block;
-		margin-top: 63upx;
-	} */
-	</script>
+</style>
