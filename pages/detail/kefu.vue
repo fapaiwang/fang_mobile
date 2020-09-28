f0a478f7f98f849018e665ce8d8f319425fabb10
cxb1988 <283817727@qq.com>
2020-09-28 16:03:04 +0800

    聊天

--- pages/detail/kefu.vue
+++ pages/detail/kefu.vue
@@ -1,8 +1,7 @@
 <template>
 	<view>
 		<!-- <web-view src="https://kefu.fangpaiwang.com/app/index.php?i=4&c=entry&toopenid=o65dpxAMaQiKzBsr_1tyFTmJDBn4&do=chat&m=cy163_customerservice"></web-view> -->
-		<web-view :src="this.online_consulting"></web-view>
-		<navTo></navTo>
+		<web-view :src="url"></web-view>
 	</view>
 </template>
 
@@ -14,9 +13,14 @@
 			}
 		},
 		onLoad(options) {
-			console.log(options);
-			if (!options.id) {
-				this.url = "/pages/deal/index?id=" + this.id
+			if (options.online_consulting) {
+				let urlLen = options.online_consulting.length
+				let _subHost = options.online_consulting.substr(0,5)
+				let _httpUrl = options.online_consulting
+				if (options.online_consulting.substr(0,5) !='https') {
+					_httpUrl = "https"+options.online_consulting.substr(4,urlLen);
+				} 
+				this.url = _httpUrl+'&c='+options.c+'&=toopenid'+options.toopenid+'&do='+options.do+'&m='+options.m
 			} else {
 				this.url = "/pages/index"
 			}
